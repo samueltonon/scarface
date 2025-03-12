416afb3b5204c5ad4c9d6a16415c03a04ea684c6
samuel <samuel@teste.com>
2025-03-11 21:24:57 -0300

    config final

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,13 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 const imagemin = require('gulp-imagemin');
+const uglify = require('gulp-uglify');
+
+function scripts() {
+    return gulp.src('./src/scripts/*.js')
+    .pipe(uglify())
+    .pipe(gulp.dest('./dist/js'))
+}
 
 function styles() {
     return gulp.src('./src/styles/*.scss')
@@ -9,13 +16,13 @@ function styles() {
 }
 
 function images() {
-    return gulp.src('./src/images/**/*')
+    return gulp.src('./src/images/**/*',  { encoding: false })
         .pipe(imagemin())
         .pipe(gulp.dest('./dist/images'));
 }
 
+exports.default = gulp.parallel(styles, images, scripts);
 
-exports.default = gulp.parallel(styles, images);
 exports.watch = function() {
     gulp.watch('./src/styles/*.scss', gulp.parallel(styles));
     gulp.watch('./src/images/**/*', gulp.parallel(images));
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -5,17 +5,6 @@ document.addEventListener('DOMContentLoaded', function () {
     const heroSection = document.querySelector('.hero');
     const alturaHero = heroSection.clientHeight;
 
-    window.addEventListener('scroll', function () {
-        const posicaoAtual = window.scrollY;
-
-        if (posicaoAtual < alturaHero) {
-            ocultaElementosDoHeader();
-        } else {
-            exibeElementosDoHeader();
-        }
-    })
-
-    // Seção atrações, programação das abas
     for (let i = 0; i < buttons.length; i++) {
         buttons[i].addEventListener('click', function (botao) {
             const abaAlvo = botao.target.dataset.tabButton;
@@ -27,21 +16,10 @@ document.addEventListener('DOMContentLoaded', function () {
         })
     }
 
-    // Seção FAQ, accord
     for (let i = 0; i < questions.length; i++) {
         questions[i].addEventListener('click', abreOuFechaResposta);
     }
 })
-
-function ocultaElementosDoHeader() {
-    const header = document.querySelector('header');
-    header.classList.add('header--is-hidden');
-}
-
-function exibeElementosDoHeader() {
-    const header = document.querySelector('header');
-    header.classList.remove('header--is-hidden');
-}
 
 function abreOuFechaResposta(elemento) {
     const classe = 'faq__questions__item--is-open';
